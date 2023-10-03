ad97ea9877666a37d08922144d04535c30f2ee9a
yousuf <u1977094@unimail.hud.ac.uk>
2023-10-03 23:05:18 +0100

    Add project files.

---
+++ NetC.JuniorDeveloperExam.Web/App_Start/RouteConfig.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace NetC.JuniorDeveloperExam.Web
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+            name: "BlogPost",
+            url: "blog/{1}",
+            defaults: new { controller = "Blog", action = "Display" }
+);
+
+        }
+    }
+}
---
+++ NetC.JuniorDeveloperExam.Web/Controller/BlogController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace YourProjectName.Controllers
+{
+    public class BlogController : Controller
+    {
+        public class BlogPost
+        {
+            public int Id { get; set; }
+            public string Title { get; set; }
+            public string Content { get; set; }
+        }
+
+
+
+        public ActionResult Display(int id)
+        {
+            List<BlogPost> blogPosts = GetBlogPosts();
+            BlogPost blogPost = blogPosts.FirstOrDefault(post => post.Id == id);
+
+            if (blogPost == null)
+            {
+                // Handle not found
+                return HttpNotFound();
+            }
+
+            return View(blogPost);
+        }
+
+        // This method simulates loading blog post data from a JSON file
+        private List<BlogPost> GetBlogPosts()
+        {
+            // Replace this with code to load and deserialize your JSON data
+            List<BlogPost> blogPosts = new List<BlogPost>
+            {
+                new BlogPost { Id = 1, Title = "Sample Blog Post 1", Content = "This is the content of the first blog post." },
+                new BlogPost { Id = 2, Title = "Sample Blog Post 2", Content = "This is the content of the second blog post." },
+                // Add more blog posts as needed
+            };
+
+            return blogPosts;
+        }
+    }
+}
